c65e215a1cd480eadcd592ac123590645672f008
713068_Ashim <ashimaakash1997@gmail.com>
2024-11-10 11:47:52 +0530

    added routes to home & coin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 //import React from 'react'
 
 import Navbar from "./components/Navbar/Navbar";
+import Coin from "./pages/Coin/Coin";
+import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin/:coinId" element={<Coin />} />
+      </Routes>
     </div>
   );
 };
