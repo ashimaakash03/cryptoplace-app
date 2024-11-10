0cd70132f47a8d73b402600a45cdc7013145f3d8
713068_Ashim <ashimaakash1997@gmail.com>
2024-11-10 16:25:42 +0530

    completed building hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,23 @@
 import "./Home.css";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="hero">
+        <h1>
+          Largest <br /> Crypto Marketplace
+        </h1>
+        <p>
+          Welcome to the largest cryptocurrency marketplace. Sign up to explore
+          more about cryptocurrency.
+        </p>
+        <form>
+          <input type="text" placeholder="Search Cryptoplace..." />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
