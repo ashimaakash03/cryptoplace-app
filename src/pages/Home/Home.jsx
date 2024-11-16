1a88724fc9e7419a253645b567c6052bdff6ef56
713068_Ashim <ashimaakash1997@gmail.com>
2024-11-16 11:06:39 +0530

    designed table header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,6 +17,15 @@ const Home = () => {
           <button type="submit">Search</button>
         </form>
       </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coin</p>
+          <p>Price</p>
+          <p>24H Change</p>
+          <p>Market Cap</p>
+        </div>
+      </div>
     </div>
   );
 };
